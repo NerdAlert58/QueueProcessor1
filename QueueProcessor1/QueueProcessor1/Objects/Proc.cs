70a97438259c6cb17d60cc3e469e3873f51aa925
ihoward <ian.howard@okstate.edu>
2019-04-14 17:28:40 -0500

    Added properties to Proc class

--- QueueProcessor1/QueueProcessor1/Objects/Proc.cs
+++ QueueProcessor1/QueueProcessor1/Objects/Proc.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 namespace QueueProcessor1.Objects
 {
     class Proc
-    {
+    {   
         public string Name { get; set; }
+        public int Priority { get; set; }
+        public int Burst { get; set; }
+        public int Arrival { get; set; }
+        public enum Color { white, blue, red, green, purple, orange, gray };
     }
 }
