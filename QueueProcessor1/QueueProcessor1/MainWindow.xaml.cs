fdbe58df0efed2a5ad6a09b9d55973706a403531
Warren Sinor <warrensinor1616@gmail.com>
2019-04-16 20:34:01 -0500

    Collecting Data from text boxes
    Scott, look for comment telling you where to push list  to call your function

--- QueueProcessor1/QueueProcessor1/MainWindow.xaml.cs
+++ QueueProcessor1/QueueProcessor1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using QueueProcessor1.Objects;
 
 namespace QueueProcessor1
 {
@@ -24,7 +25,7 @@ namespace QueueProcessor1
         {
             InitializeComponent();
 
-            for (int j = 0; j < 15; j++)
+         /*   for (int j = 0; j < 15; j++)
             {
                 Button MyControl1 = new Button();
                 MyControl1.Content =j.ToString();
@@ -34,12 +35,56 @@ namespace QueueProcessor1
                 queuegrid.ColumnDefinitions.Add(new ColumnDefinition());
                 queuegrid.Children.Add(MyControl1);
 
-            }
+            }*/
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void buttonCalc (object sender, RoutedEventArgs e)
         {
+            Proc p0 = new Proc();
+            p0.Burst = Convert.ToInt32(burst1.Text);
+            p0.Priority = Convert.ToInt32(priority1.Text);
+            p0.Arrival = Convert.ToInt32(arrival1.Text);
+            p0.Name = "P0";
 
+            Proc p1 = new Proc();
+            p1.Burst = Convert.ToInt32(burst2.Text);
+            p1.Priority = Convert.ToInt32(priority2.Text);
+            p1.Arrival = Convert.ToInt32(arrival2.Text);
+            p1.Name = "P1";
+
+            Proc p2 = new Proc();
+            p2.Burst = Convert.ToInt32(burst3.Text);
+            p2.Priority = Convert.ToInt32(priority3.Text);
+            p2.Arrival = Convert.ToInt32(arrival3.Text);
+            p2.Name = "P2";
+
+            Proc p3 = new Proc();
+            p3.Burst = Convert.ToInt32(burst4.Text);
+            p3.Priority = Convert.ToInt32(priority4.Text);
+            p3.Arrival = Convert.ToInt32(arrival4.Text);
+            p3.Name = "P3";
+
+            Proc p4 = new Proc();
+            p4.Burst = Convert.ToInt32(burst5.Text);
+            p4.Priority = Convert.ToInt32(priority5.Text);
+            p4.Arrival = Convert.ToInt32(arrival5.Text);
+            p4.Name = "P4";
+
+            Proc p5 = new Proc();
+            p5.Burst = Convert.ToInt32(burst6.Text);
+            p5.Priority = Convert.ToInt32(priority6.Text);
+            p5.Arrival = Convert.ToInt32(arrival6.Text);
+            p5.Name = "P5";
+
+            List<Proc> procList = new List<Proc>();
+            procList.Add(p0);
+            procList.Add(p1);
+            procList.Add(p2);
+            procList.Add(p3);
+            procList.Add(p4);
+            procList.Add(p5);
+
+            //scott, call your function here
         }
     }
 }
